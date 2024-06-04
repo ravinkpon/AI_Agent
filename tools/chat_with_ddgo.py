f04c40e698957573dbf6c4c4d6c1992c6cf6e2e9
ravinkpon <ravinkpon@gmail.com>
2024-06-04 15:16:31 +0100

    fixed call back

--- tools/chat_with_ddgo.py
+++ tools/chat_with_ddgo.py
@@ -5,10 +5,11 @@ from langchain_community.tools import DuckDuckGoSearchRun
 
 
 class Duckduckgo:
-    def __init__(self,llm_selected,provider_selected,unify_key):
+    def __init__(self,llm_selected,provider_selected,unify_key,messages):
         self.llm_selected = llm_selected
         self.provider_selected = provider_selected
         self.unify_key = unify_key
+        self.messages = messages
         self.creat_agent()
         
         
@@ -17,31 +18,33 @@ class Duckduckgo:
         self.tools = [DuckDuckGoSearchRun(name="Search")]
         self.chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=self.llm, tools=self.tools)
         self.memory=ConversationBufferMemory(
-                return_messages=True, memory_key="chat_history", output_key="output"
+                chat_memory=self.messages  ,return_messages=True, memory_key="chat_history", output_key="output"
             )
         self.executor = AgentExecutor.from_agent_and_tools(
                         agent=self.chat_agent,
                         tools=self.tools,
-                        #verbose=True,
+                        verbose=True,
                         memory=self.memory,
                         handle_parsing_errors=True,
+                        return_intermediate_steps=True #testing retunring intermediate steps
                     )
             #return executor.invoke({"input":prompt})
     
-    def run(self,prompt):
-        response = self.executor.invoke({"input": prompt})
+    def run(self,prompt,**callbacks):
+        response = self.executor.invoke({"input": prompt},callbacks)
         return response
     
     
 
 if __name__ == "__main__":
-    duckduckgo = Duckduckgo('llm_selected', 'provider_selected', 'unify_key')
+    duckduckgo = Duckduckgo('llm_selected', 'provider_selected', 'unify_key',",messages")
     response = duckduckgo.run('prompt')
 
 
 
 # # Invoking the executor with the user prompt
-# response = executor.invoke({"input": prompt})
+# ddg = Duckduckgo('gpt-4-turbo', 'openai', 'jOIetrH1GL2QWUj5iZO5XVC6gR+VXm9pnYZE7y7ALNU=')
+# print(ddg.run('What is the capital of India?'))
     
 # # Displaying the response in the Streamlit app
 # st.chat_message("assistant").write(response["output"])
