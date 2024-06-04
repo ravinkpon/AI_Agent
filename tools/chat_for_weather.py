2aa6806005c54587c480490d44854fb14ee7d604
ravinkpon <ravinkpon@gmail.com>
2024-06-04 15:16:06 +0100

    fixed memory call back and imporved agent responce

--- tools/chat_for_weather.py
+++ tools/chat_for_weather.py
@@ -1,14 +1,15 @@
 from langchain.memory import ConversationBufferMemory
 from langchain_unify.chat_models import ChatUnify
-from langchain.agents import ConversationalChatAgent, AgentExecutor,load_tools
+from langchain.agents import ConversationalChatAgent, AgentExecutor,load_tools,AgentType
 import os
 
 
 class OpenWeatherMap:
-    def __init__(self,llm_selected,provider_selected,unify_key,openweathermap_api_key):
+    def __init__(self,llm_selected,provider_selected,unify_key,openweathermap_api_key,messages):
         self.llm_selected = llm_selected
         self.provider_selected = provider_selected
         self.unify_key = unify_key
+        self.messages=messages
         #self.openweathermap_api_key = openweathermap_api_key
         os.environ["OPENWEATHERMAP_API_KEY"] = openweathermap_api_key
         self.create_agent()
@@ -18,19 +19,20 @@ class OpenWeatherMap:
         self.tools = load_tools(["openweathermap-api"],self.llm)
         self.chat_agent = ConversationalChatAgent.from_llm_and_tools(llm=self.llm, tools=self.tools)
         self.memory=ConversationBufferMemory(
-                return_messages=True, memory_key="chat_history", output_key="output"
+                chat_memory=self.messages, return_messages=True, memory_key="chat_history", output_key="output"
             )
         self.executor = AgentExecutor.from_agent_and_tools(
                         agent=self.chat_agent,
                         tools=self.tools,
+                        agenttype=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
                         #verbose=True,
                         memory=self.memory,
                         handle_parsing_errors=True,
                     )
             #return executor.invoke({"input":prompt})
     
-    def run(self,prompt):
-        response = self.executor.invoke({"input": prompt})
+    def run(self,prompt,**callbacks):
+        response = self.executor.invoke({"input": prompt},callbacks)
         return response
 
 if __name__ == "__main__":
